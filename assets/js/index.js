b8f1206fea44156058d21f9c36f3a402433a51d6
Ikigai <alvardsandz@gmail.com>
2024-05-19 00:33:28 +0200

    create script

---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+const ball = document.getElementById('ball');
+const playBtn = document.getElementById('play');
+const pauseBtn = document.getElementById('pause');
+const reverseBtn = document.getElementById('reverse');
+const speedUpBtn = document.getElementById('speed-up');
+const slowDownBtn = document.getElementById('slow-down');
+
+const rollAnimation = [
+  {
+    transform: 'rotate(0) translate3D(-50%, -50%, 0)',
+    color: 'white',
+  },
+  {
+    color: 'blue',
+    offset: 0.3,
+  },
+  {
+    transform: 'rotate(360deg) translate3D(-50%, -50%, 0)',
+    color: 'white',
+  },
+];
+
+const rollOptions = {
+  duration: 3000,
+  iterations: Infinity,
+};
+
+const roll = ball.animate(rollAnimation, rollOptions);
+
+playBtn.addEventListener('click', () => {
+  roll.playbackRate = 1;
+  roll.play();
+});
+
+pauseBtn.addEventListener('click', () => roll.pause());
+reverseBtn.addEventListener('click', () => roll.reverse());
+
+speedUpBtn.addEventListener('click', () => {
+  roll.playbackRate = roll.playbackRate * 2;
+});
+
+slowDownBtn.addEventListener('click', () => {
+  roll.playbackRate = roll.playbackRate * 0.5;
+});
